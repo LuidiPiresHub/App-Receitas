7c25d5386c702cdc3cca17d16e00bb2b32fc0533
Luídi <luidihot@gmail.com>
2022-09-23 17:48:36 -0300

    Base do Projeto

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import RecipesAppProvider from './Context/RecipesAppProvider';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(
-    <RecipesAppProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </RecipesAppProvider>,
-  );
+ReactDOM.render(
+  <RecipesAppProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RecipesAppProvider>,
+  (document.getElementById('root')),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
