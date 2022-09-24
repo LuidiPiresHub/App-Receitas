e7db85a0d720206484993cfc3348c96ca064a6a1
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-09-24 00:10:36 -0300

    lint

--- src/Context/RecipesAppProvider.jsx
+++ src/Context/RecipesAppProvider.jsx
@@ -5,7 +5,7 @@ import RecipesAppContext from './RecipesAppContext';
 const INITIAL_STATE = { nome: 'Xablau', idade: 100 };
 
 export default function RecipesAppProvider({ children }) {
-  const [state, setState] = useState(INITIAL_STATE);
+  const [state] = useState(INITIAL_STATE);
   return (
     <RecipesAppContext.Provider value={ state }>
       {children}
