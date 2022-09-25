c5e3f79b1506752b87c4e932449b967feecb519b
Raquel_Gomes <raquel.wurzler@hotmail.com>
2022-09-25 17:03:46 -0300

    Questions two and three done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,6 +4,25 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <form>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        data-testid="email-input"
+      />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        data-testid="password-input"
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </form>
   );
 }
