57c962aafc2c1d2600fa1e19f3a186b9ffe47bc7
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-09-30 17:54:04 -0300

    rebase completo com correção da recipe details atualizada

--- src/Pages/RecipeDetails.jsx
+++ src/Pages/RecipeDetails.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Carousel from '../Components/Carousel';
 import Favorite from '../Components/Favorite';
 import ProgressSection from '../Components/ProgressSection';
 import {
@@ -43,26 +44,91 @@ export default function RecipeDetails({ location: { pathname }, location }) {
   const { id } = useParams();
   const [returnFetch, setReturnFetch] = useState([]);
   const [type, setType] = useState('');
+  const [carousel, setCarousel] = useState([]);
 
+  const callingFetch = async () => {
+    if (pathname.includes('meals')) {
+      setType('meals');
+      const resultMeals = await fetchApiMeals(id);
+      setReturnFetch(resultMeals);
+      const resultDrinks = await fetchAllDrinks();
+      setCarousel(
+        resultDrinks.drinks.filter((drink, index) => index < DISPLAY_LIMIT),
+      );
+    } else if (pathname.includes('drinks')) {
+      setType('drinks');
+      const resultDrinks = await fetchApiDrinks(id);
+      setReturnFetch(resultDrinks);
+      const resultMeals = await fetchAllMeals();
+      setCarousel(
+        resultMeals.meals.filter((drink, index) => index < DISPLAY_LIMIT),
+      );
+    }
+  };
   useEffect(() => {
-    const callingFetch = async () => {
-      if (pathname.includes('meals')) {
-        setType('meals');
-        const meals = await fetchApiMeals(id);
-        setReturnFetch(meals);
-      } else if (pathname.includes('drinks')) {
-        setType('drinks');
-        const drinks = await fetchApiDrinks(id);
-        setReturnFetch(drinks);
-      }
-    };
     callingFetch();
-  }, []);
+  }, [pathname]);
 
+  const foodType = whatFood(pathname);
+
+  if (returnFetch.length > 0 && carousel.length > 0) {
+    const ingredientsMeasures = getIngredientsAndMeasures(returnFetch);
+    return (
+      <main>
+        <header>{foodType}</header>
+        <section>
+          <h1 data-testid="recipe-title">{returnFetch[0][`str${foodType}`]}</h1>
+          <img
+            src={ returnFetch[0][`str${foodType}Thumb`] }
+            alt={ `${foodType}_img` }
+            data-testid="recipe-photo"
+            width="300px"
+            height="300px"
+          />
+          {foodType === 'Meal' ? (
+            <h3 data-testid="recipe-category">{returnFetch[0].strCategory}</h3>
+          ) : (
+            <h3 data-testid="recipe-category">{returnFetch[0].strAlcoholic}</h3>
+          )}
+          <ul>
+            {ingredientsMeasures.map((ingredient, index) => (
+              <li
+                key={ `${ingredient}-${index}` }
+                data-testid={ `${index}-ingredient-name-and-measure` }
+              >
+                {ingredient}
+              </li>
+            ))}
+          </ul>
+          <p data-testid="instructions">{returnFetch[0].strInstructions}</p>
+          {foodType === 'Meal' ? (
+            <iframe
+              title={ `${foodType}-Preparation` }
+              width="420"
+              height="315"
+              allowFullScreen
+              data-testid="video"
+              src={ returnFetch[0].strYoutube.replace('watch?v=', 'embed/') }
+            />
+          ) : (
+            ''
+          )}
+        </section>
+        <section style={ { width: '100vw', overflowX: 'hidden' } }>
+          <Favorite location={ location } returnFetch={ returnFetch } />
+          <Carousel carousel={ carousel } />
+          <ProgressSection
+            type={ foodType === 'Meal' ? 'meals' : 'drinks' }
+            id={ id }
+          />
+        </section>
+      </main>
+    );
+  }
   return (
     <section>
+      <Favorite location={ location } returnFetch={ returnFetch } />
       <ProgressSection recipeObj={ returnFetch[0] } type={ type } id={ id } />
-      <Favorite location={ location } returnFetch={ returnFetch } id={ id } />
     </section>
   );
 }
